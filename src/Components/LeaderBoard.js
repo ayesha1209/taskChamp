6e980bc9af1c0e8cb4aa22c90a66541fcdc84aea
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2024-11-13 15:36:06 +0530

    Learder-UI-2

--- src/Components/LeaderBoard.js
+++ src/Components/LeaderBoard.js
@@ -78,13 +78,13 @@ const LeaderBoard = () => {
   const getReward = (index) => {
     switch (index) {
       case 0:
-        return <span className="text-amber-500">🏆 Gold Medal</span>;
+        return <span className="text-gray-300">🏆 Gold Medal</span>;
       case 1:
-        return <span className="text-slate-800">🥈 Silver Medal</span>;
+        return <span className="text-gray-300">🥈 Silver Medal</span>;
       case 2:
-        return <span className="text-orange-800">🥉 Bronze Medal</span>;
+        return <span className="text-gray-300">🥉 Bronze Medal</span>;
       default:
-        return <span className="text-gray-500">🎖 Participation Badge</span>;
+        return <span className="text-gray-300">🎖 participant</span>;
     }
   };
 
@@ -98,59 +98,63 @@ const LeaderBoard = () => {
           className={`container mx-auto ${styles.leader_outer}`}
           style={{ paddingLeft: "250px" }}
         >
-          <div className={`px-32 py-8 w-[100%] ${styles.leader_inner}`}>
+          <div className={`px-20 py-8 w-[100%] ${styles.leader_inner}`}>
             <h1 className={styles.heading}>LeaderBoard</h1>
             <h1 className={styles.heading}>LeaderBoard</h1>
             {/* Logged-in User's Rank */}
             {loggedInUserRank ? (
               <div
-                className={`bg-white shadow-lg rounded-lg mb-6 ${styles.rank_outer}`}
+                className={`bg-white shadow-lg rounded-lg mb-10 ${styles.rank_outer}`}
               >
                 <div className={`p-4 ${styles.rank_inner}`}>
                   <h2 className="text-xl font-bold text-white mb-1">
-                    Your Rank: {loggedInUserRank.rank}
+                    Your Rank : {loggedInUserRank.rank}
                   </h2>
                   <p className="text-gray-400 text-lg font-semibold">
-                    Username: {loggedInUserRank.username}
+                    Username : {loggedInUserRank.username}
                   </p>
                   <p className="text-gray-400 text-lg font-semibold">
-                    Tasks Completed: {loggedInUserRank.completedTasks}
+                    Tasks Completed : {loggedInUserRank.completedTasks}
                   </p>
                   <p className="text-gray-400 text-lg font-semibold">
-                    Score: {loggedInUserRank.score}
+                    Score : {loggedInUserRank.score}
                   </p>
-                </div>{" "}
+                </div>
               </div>
             ) : (
-              <div className="bg-white shadow-lg rounded-lg p-4 mb-6">
-                <h2 className="text-xl font-bold mb-2">Your Rank: N/A</h2>
+              <div className="bg-white shadow-lg rounded-lg p-4 mb-10">
+                <h2 className="text-xl font-bold mb-2">Your Rank : N/A</h2>
                 <p className="text-gray-600 text-sm">
                   Could not find your data in the leaderboard.
                 </p>
               </div>
             )}
             {/* Top 3 Contributors */}
-            <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 mb-9">
+            <div className={`grid grid-cols-1 sm:grid-cols-3 gap-10 mb-9`}>
               {users.slice(0, 3).map((user, index) => (
                 <div
                   key={user.userId}
-                  className={`bg-white shadow-xl text-sm rounded-2xl p-4 flex flex-col mr-4 items-center hover:cursor-pointer 
-            transition-transform duration-300 hover:scale-105 ${
+                  className={`shadow-xl text-sm rounded-2xl flex flex-col mr-4 items-center hover:cursor-pointer 
+            transition-transform duration-1000 hover:scale-105 ${
               index === 0
-                ? "border-t-4 border-yellow-400"
+                ? "border-t-4 border-[#9793ba]"
                 : index === 1
-                ? "border-t-4 border-gray-400"
-                : "border-t-4 border-orange-400"
-            }`}
+                ? "border-t-4 border-#9793ba"
+                : "border-t-4 border-#9793ba"
+            } ${styles.rank_board_outer}`}
                 >
-                  <h2 className="text-lg font-bold mb-1">{user.username}</h2>
-                  <p className="text-gray-600 font-semibold">
-                    Tasks Completed: {user.completedTasks}
-                  </p>
-                  <p className="text-gray-600 font-semibold">
-                    Score: {user.score}
-                  </p>
-                  <div className="mt-3 font-semibold">{getReward(index)}</div>
+                  <div className={`${styles.rank_board_inner} p-5`}>
+                    <h2 className="text-lg text-white font-bold mb-1">
+                      {user.username}
+                    </h2>
+                    <p className="text-gray-400 font-semibold">
+                      Tasks Completed: {user.completedTasks}
+                    </p>
+                    <p className="text-gray-400 font-semibold">
+                      Score: {user.score}
+                    </p>
+                    <div className="mt-3 font-semibold">{getReward(index)}</div>
+                  </div>
                 </div>
               ))}
             </div>
